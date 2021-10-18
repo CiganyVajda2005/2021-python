d97ad6b05458b995c0c10b389489dd863bb0d815
Kristóf Szabó <90602057+CiganyVajda2005@users.noreply.github.com>
2021-10-18 10:36:31 +0200

    Create szavak.py

---
+++ szavak.py
@@ -0,0 +1,12 @@
+szo1 = input('Első szó: ')
+szo2 = input('Második szó: ')
+szo1_hossza = len (szo1)
+szo2_hossza = len(szo2)
+if (szo1_hossza > szo2_hossza):
+    print ('az 1 a hoszab szó' + szo1)
+elif szo2_hossza==szo1_hossza:
+    print('egyenlő a két szám')
+    else:
+print ('a második a nagyobb')
+
+
