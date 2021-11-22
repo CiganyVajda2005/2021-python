b345b6909a28425f92cba76f3642528fca8b260b
Kristóf Szabó <90602057+CiganyVajda2005@users.noreply.github.com>
2021-11-22 10:33:27 +0100

    Add files via upload

---
+++ dik.py
@@ -0,0 +1,49 @@
+#feladat 11
+for x in range (1,11):
+    print(x)
+#fealadat 12
+for x1 in range (0,11,2):
+    print(x1)
+ 
+ #feladat 13
+for x2 in range (0,101,3):
+    print(x2)
+
+#feladat 14
+x3 = int(input("egy számot irjá ")) 
+print (x3*2)
+
+#feladat 15
+print (0+1+2+3+4)
+
+#feladat 16
+for x5 in range (1,11):
+    print(x5*x5)
+
+#feladat 17
+    for x6 in range (0,31):
+        print(x6*x6)
+ 
+ #feldat 18
+    for fasz in range (0,101,2):
+        print (fasz)
+
+        #feladat 19
+szam1 = int(input("egy számot irjá ")) 
+print (szam1*szam1)
+
+#feladat 20
+for allah in range (50,10,-2):
+        print (allah)
+
+        #feladat 21
+a = int(input("egy szám ami a területe lesz  ")) 
+b = int(input( " a 2. szám ami a területe lesz  ")) 
+print ( a*b,'ez a négyzet területe ')
+
+a1 = int(input("egy szám ami a kerülete lesz  ")) 
+print (2*(a+b) ,'ez a téglalap kerülete ')
+
+#feladat 22
+
+
