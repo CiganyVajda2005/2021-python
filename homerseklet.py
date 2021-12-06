8546c28d16c020f22f6a995e81d4f85cfd59508f
Kristóf Szabó <90602057+CiganyVajda2005@users.noreply.github.com>
2021-12-06 10:21:08 +0100

    Add files via upload

--- homerseklet.py
+++ homerseklet.py
@@ -1,11 +1,13 @@
-def homerseklet (C):
+def temp (C):
     if C < 0 :
         print ('-foksz van tesomsz')
     elif C <= 0 :
         print ('0 foksz van most')
     elif  C >= 100:
         print('nagy foksz van tesomsz')
+    else: 
+        print('jó foksz van') 
 
-for x in range (-20,121,20):
-    print (x,"c",)
-    homerseklet (x)
+for C in range (-20,121,20):
+    print (C,"C " , end="")
+    temp(C)
